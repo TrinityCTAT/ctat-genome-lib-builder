52d27198713f06892332dee03a0946b72aa449ef
Brian Haas <brianjohnhaas@gmail.com>
2020-04-01 12:20:08 -0400

    remove all annotated readthrus

--- util/revise_gencode_annotations.pl
+++ util/revise_gencode_annotations.pl
@@ -63,10 +63,11 @@ main: {
                                   -checkpoint_dir => $tmpdir);
 
     ## filter out the IG features that we'll add back later as super-loci
+    ## also removing annotations flagged as readthru_transcript Apr-01-2020 
     my $feature_selected_gtf = "$tmpdir/" . basename($gtf_file) . ".feature_selected";
-    my $cmd = "bash -c \"set -euxo pipefail; cat $gtf_file |  egrep -v 'IG_V_gene|IG_C_gene|IG_D_gene|IG_J_gene' > $feature_selected_gtf\" ";
+    my $cmd = "bash -c \"set -euxo pipefail; cat $gtf_file |  egrep -v 'IG_V_gene|IG_C_gene|IG_D_gene|IG_J_gene|readthrough_transcript' > $feature_selected_gtf\" ";
     $pipeliner->add_commands(new Command($cmd, "feature_selection.ok"));
-        
+    
     ## remove readthru transcripts
     my $no_readthrus_gtf = "$feature_selected_gtf.noreadthrus";
     $cmd = "$UTILDIR/remove_long_intron_readthru_transcripts.pl $feature_selected_gtf 100000 > $no_readthrus_gtf";
